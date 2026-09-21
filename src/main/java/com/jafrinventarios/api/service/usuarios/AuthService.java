717eae2f60c272e4f21bed993884a749fd7d07a9
johnalexf <johnalexfr@gmail.com>
2026-09-21 18:18:44 -0500

    feat(service/usuarios): crear AuthService e implementar logica de autenticacion

---
+++ src/main/java/com/jafrinventarios/api/service/usuarios/AuthService.java
@@ -0,0 +1,33 @@
+package com.jafrinventarios.api.service.usuarios;
+
+import com.jafrinventarios.api.entity.usuarios.Usuario;
+import com.jafrinventarios.api.repository.usuarios.UsuarioRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Usuario autenticarUsuario(String correo, String contrasenaPlana) throws Exception {
+
+        Optional<Usuario> usuario = usuarioRepository.findByCorreoUsuario( correo );
+
+        if(usuario.isEmpty())
+            throw new Exception("Credenciales invalidas");
+
+        if( !passwordEncoder.matches( contrasenaPlana, usuario.get().getContrasenaUsuario() ) )
+            throw new Exception("Credenciales invalidas");
+
+        return usuario.get();
+    }
+}
