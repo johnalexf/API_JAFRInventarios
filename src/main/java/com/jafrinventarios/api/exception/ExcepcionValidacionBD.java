b6432f8f1bbd051f1ea43897de8baac4aeb62a4f
johnalexf <johnalexfr@gmail.com>
2026-09-21 16:59:01 -0500

    feat(excepciones): crear clase ExcepcionValidacionBD para manejo de errores personalizados

---
+++ src/main/java/com/jafrinventarios/api/exception/ExcepcionValidacionBD.java
@@ -0,0 +1,40 @@
+package com.jafrinventarios.api.exception;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Excepción personalizada para transportar errores de validación
+ * (como duplicados o reglas de negocio) desde la capa de Servicios
+ * hacia los Controladores.
+ */
+public class ExcepcionValidacionBD extends Exception {
+
+    private final HashMap<String, String> errores;
+
+    public ExcepcionValidacionBD(HashMap<String, String> errores) {
+        // Le pasamos un mensaje genérico al padre por si se imprime en consola
+        super("Existen errores de validacion en la base de datos ");
+        this.errores = errores;
+    }
+
+    public HashMap<String, String> getErrores() {
+        return errores;
+    }
+
+    public String getErroresEnString(){
+        StringBuilder erroresString = new StringBuilder();
+        for ( Map.Entry< String, String> error : this.errores.entrySet() ) {
+            erroresString.append("\n ( ")
+                    .append(error.getKey())
+                    .append(" : ")
+                    .append(error.getValue())
+                    .append(" ) ");
+
+        }
+        erroresString.append("\n");
+        return erroresString.toString();
+    }
+
+}
